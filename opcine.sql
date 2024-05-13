61c6278899d9410724c8e90595c34382a37a5486
Srđan Filipović <srdjanfilipovic991@gmail.com>
2024-05-13 16:15:43 +0200

    opcine sql

---
+++ opcine.sql
@@ -0,0 +1,15 @@
+﻿use master;
+go
+drop database if exists opcine;
+go
+create database opcine;
+go
+
+use opcine;
+
+create table nacelnici(
+sifra int not null primary key identity(1,1),
+ime varchar (50) not null,
+prezime varchar(50) not null,
+datumRodenja datetime
+);
