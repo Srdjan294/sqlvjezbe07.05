c0f093f36a9dae10da46862e87ac8d5eda719ca7
Srđan Filipović <srdjanfilipovic991@gmail.com>
2024-05-13 16:52:26 +0200

    samostan sql

---
+++ samostan.sql
@@ -0,0 +1,17 @@
+﻿use master;
+go
+drop database if exists samostan;
+go
+create database samostan;
+go
+
+use samostan;
+
+create table svecenici(
+sifra int not null primary key identity(1,1),
+ime varchar(100) not null,
+prezime varchar(100) not null,
+nadredeni int not null
+);
+
+alter table svecenici add foreign key (nadredeni) references svecenici(sifra);
